7c2978aa4706e4e5fa26b1e71c60cb249d8d7fcd
Martin <nitromagix@gmail.com>
2022-09-07 19:42:21 -0700

    finished everything in ppp-2-more-python-function-features EXCEPT recursive_deduplicate

--- ppp-2-more-python-function-features.py
+++ ppp-2-more-python-function-features.py
@@ -48,14 +48,36 @@ def recursive_deduplicate(s, *args):
     return str(s[0]) + str(s[1]) + recursive_deduplicate(s[2::])
 
 
-# def recursive_deduplicate2(s, *args):
-#     # if len(s) <= 1 and args[0] and len(args[0] == 0):
-#     #     return s
-    
-    
+#        V     copied from solution (doesn't work)     V
 
+def recursive_deduplicate2(my_str, i=0):
+    # if our string is empty or only has 1 thing, it's got no duplicates
+    # if i is at the end of the string, no duplicates are left
+    if len(my_str) <= 1 or i == len(my_str)-1:
+        return my_str
+    else:
+        # str at current position is same as next position
+        if my_str[i] == my_str[i+1]:
+            return recursive_deduplicate(my_str[0:i]+my_str[i+1:], i)
+        else:
+            # no duplicate at current position, check next
+            return recursive_deduplicate(my_str, i+1)
 
 
 print(recursive_deduplicate("AABBCC"))
 print(recursive_deduplicate("AAACC"))
 print(recursive_deduplicate("ABCDEFG"))
+
+
+print(recursive_deduplicate2("AABBCC"))
+print(recursive_deduplicate2("AAACC"))
+print(recursive_deduplicate2("ABCDEFG"))
+
+
+
+def recursive_reverse(s):
+    if len(s) <= 1:
+        return s
+    return s[-1] + recursive_reverse(s[:-1])
+
+print(recursive_reverse("do you think this will work?"))
